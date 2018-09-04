9e3db8e939d9d5e4c14582d1923a34a098bf83cb
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2018-09-03 22:11:39 -0300

    [WIP] Implement contiguous and non-contiguous clues

--- tests/model/board_test.cpp
+++ tests/model/board_test.cpp
@@ -61,6 +61,15 @@ inline Board createNonContiguousRectangularBoard() {
                         ->build();
 }
 
+// inline Board createContiguousRectangularBoard() {
+//   return BoardBuilder(2).column({2, 1}, { true })
+//                         ->column({1, 2}, { false, true })
+//                         ->row({1, 1})
+//                         ->row({1, 1})
+//                         ->row({1, 1})
+//                         ->build();
+// }
+
 inline Board createSquareBoard() {
   return createSquareBoardWithDiagonals();
 }
@@ -230,6 +239,120 @@ TEST(Board, ShouldBeInvalidWhenHorizontalLinesArePartiallyFilledIncorrectly) {
 
   ASSERT_EQ(board.isValid(), false);
 }
+
+// TEST(Board, ShouldBeValidWhenNonContiguousRectangularBoardIsFilledCorrectly) {
+//   Board board = createNonContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C1;
+//   board.results[0][2] = C0;
+//   board.results[1][0] = C1;
+//   board.results[1][1] = C0;
+//   board.results[1][2] = C1;
+//
+//   ASSERT_EQ(board.isValid(), true);
+// }
+//
+// TEST(Board, ShouldBeInvalidWhenNonContiguousRectangularBoardIsFilledWithIncorrectAmounts) {
+//   Board board = createNonContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C1;
+//   board.results[0][2] = C1;
+//   board.results[1][0] = C1;
+//   board.results[1][1] = C0;
+//   board.results[1][2] = C1;
+//
+//   ASSERT_EQ(board.isValid(), false);
+// }
+//
+// TEST(Board, ShouldBeInvalidWhenNonContiguousRectangularBoardIsFilledInInvalidPlaces) {
+//   Board board = createNonContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C0;
+//   board.results[0][2] = C1;
+//   board.results[1][0] = C1;
+//   board.results[1][1] = C1;
+//   board.results[1][2] = C0;
+//
+//   ASSERT_EQ(board.isValid(), false);
+// }
+//
+// TEST(Board, ShouldBeInvalidWhenNonContiguousRectangularBoardIsPartiallyFilledWithIncorrectAmounts) {
+//   Board board = createNonContiguousRectangularBoard();
+//
+//   board.results[0][1] = C1;
+//   board.results[1][1] = C1;
+//
+//   ASSERT_EQ(board.isValid(), false);
+// }
+// 
+// TEST(Board, ShouldBeInvalidWhenNonContiguousRectangularBoardIsPartiallyFilledInInvalidPlaces) {
+//   Board board = createNonContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C0;
+//
+//   ASSERT_EQ(board.isValid(), false);
+// }
+//
+// TEST(Board, ShouldBeValidWhenContiguousRectangularBoardIsFilledCorrectly) {
+//   Board board = createContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C0;
+//   board.results[0][2] = C1;
+//   board.results[1][0] = C1;
+//   board.results[1][1] = C1;
+//   board.results[1][2] = C0;
+//
+//   ASSERT_EQ(board.isValid(), true);
+// }
+//
+// TEST(Board, ShouldBeInvalidWhenContiguousRectangularBoardIsFilledWithIncorrectAmounts) {
+//   Board board = createContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C1;
+//   board.results[0][2] = C1;
+//   board.results[1][0] = C1;
+//   board.results[1][1] = C1;
+//   board.results[1][2] = C0;
+//
+//   ASSERT_EQ(board.isValid(), false);
+// }
+//
+// TEST(Board, ShouldBeInvalidWhenContiguousRectangularBoardIsFilledInInvalidPlaces) {
+//   Board board = createContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C1;
+//   board.results[0][2] = C0;
+//   board.results[1][0] = C1;
+//   board.results[1][1] = C0;
+//   board.results[1][2] = C1;
+//
+// //   ASSERT_EQ(board.isValid(), false);
+// // }
+//
+// TEST(Board, ShouldBeInvalidWhenContiguousRectangularBoardIsPartiallyFilledWithIncorrectAmounts) {
+//   Board board = createContiguousRectangularBoard();
+//
+//   board.results[0][0] = C1;
+//   board.results[0][1] = C1;
+//
+//   ASSERT_EQ(board.isValid(), false);
+// }
+//
+// TEST(Board, ShouldBeInvalidWhenContiguousRectangularBoardIsPartiallyFilledInInvalidPlaces) {
+//   Board board = createContiguousRectangularBoard();
+//
+//   board.results[0][0] = C0;
+//   board.results[0][1] = C0;
+//
+//   ASSERT_EQ(board.isValid(), false);
+// }
 
 //TODO: Tests for  ushort countColorIn{Column,Row}
 //TODO: Tests for  ushort clueFor{Column,Row}
