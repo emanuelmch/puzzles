f747d9794042ec8af965235f812b670e133e0bd4
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2020-09-12 16:24:31 -0300

    [3/3] Optimize Maths::isPrime
    Instead of checking every number for divisibility, it should be enough
    to check only the even numbers, and only up to half the target.

--- src/maths/primes.h
+++ src/maths/primes.h
@@ -28,7 +28,9 @@ static bool isPrime(long n) {
   if (n == 1) return false;
   if (n == 2) return true;
 
-  for (long i = 2; i < n; ++i) {
+  if (n % 2 == 0) return false;
+
+  for (long i = 3; i < (n / 2); i += 2) {
     if (n % i == 0) return false;
   }
 
