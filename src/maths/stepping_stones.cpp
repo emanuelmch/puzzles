b81379ffad493db5e39ebb8dee5bdb1e7fe8090b
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2022-01-17 21:39:24 -0300

    [1/3] Add the Stepping Stones boilerplate and tests

---
+++ src/maths/stepping_stones.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 Emanuel Machado da Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "stepping_stones.h"
+
+#include "common/assertions.h" // ensure
+
+#include <array>
+
+uintmax_t Maths::steppingStones(uint8_t start) {
+  constexpr std::array<uint8_t, 6> results{1, 16, 28, 38, 49, 60};
+  ensure(start >= 1);
+  ensure(start <= results.size());
+  return results[start - 1];
+}
