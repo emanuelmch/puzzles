6f5cd55c08c71c2af190861d74a15c3bc9e9fb90
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2022-01-22 14:54:51 -0300

    [2/3] Add the first version of the Stepping Stones algorithm
    This one only works with the n=1 case, so I don't have to write the code
    for putting all stones in yet.

--- src/maths/stepping_stones.cpp
+++ src/maths/stepping_stones.cpp
@@ -25,10 +25,105 @@
 #include "common/assertions.h" // ensure
 
 #include <array>
+#include <map>
+#include <queue>
+#include <set>
 
-uintmax_t Maths::steppingStones(uint8_t start) {
-  constexpr std::array<uint8_t, 6> results{1, 16, 28, 38, 49, 60};
-  ensure(start >= 1);
-  ensure(start <= results.size());
-  return results[start - 1];
+namespace {
+using Point = std::pair<int8_t, int8_t>;
+struct Node {
+  std::map<Point, uint8_t> board;
+  uintmax_t max;
+
+  Node() : board{}, max{0} { apply(std::make_pair(0, 0), 1); }
+
+  [[nodiscard]] std::set<Point> allPossibleMoves() const {
+    std::set<Point> moves;
+
+    for (auto element : board) {
+      auto center = element.first;
+      for (auto j = -1; j <= 1; ++j) {
+        for (auto i = -1; i <= 1; ++i) {
+          if (i == 0 && j == 0) continue;
+
+          Point newPoint{center.first + i, center.second + j};
+          if (board.contains(newPoint) == false) {
+            moves.insert(newPoint);
+          }
+        }
+      }
+    }
+
+    return moves;
+  }
+
+  [[nodiscard]] uint8_t getOrZero(const Point &point) const {
+    auto it = board.find(point);
+    if (it != board.end()) {
+      return it->second;
+    } else {
+      return 0;
+    }
+  }
+
+  [[nodiscard]] uintmax_t sum(Point center) const {
+    uintmax_t sum = 0;
+
+    for (auto j = -1; j <= 1; ++j) {
+      for (auto i = -1; i <= 1; ++i) {
+        if (i == 0 && j == 0) continue;
+
+        Point newPoint{center.first + i, center.second + j};
+        sum += getOrZero(newPoint);
+      }
+    }
+
+    return sum;
+  }
+
+  void apply(Point point, uintmax_t height) {
+    ensure(height == max + 1);
+    board.emplace(point, height);
+    max = height;
+  }
+};
+}
+
+uintmax_t Maths::steppingStones(uint8_t startingStones) {
+  ensure(startingStones >= 1);
+
+  // Temporary hardcoded results
+  constexpr std::array<uint8_t, 6> results{0, 16, 28, 38, 49, 60};
+  ensure(startingStones <= results.size());
+  if (results[startingStones - 1] != 0) {
+    return results[startingStones - 1];
+  }
+  // Now here is the actual algorithm
+  ensure(startingStones == 1); // We only support 1 stone for now
+  Node highestNode;
+
+  std::queue<Node> nodes;
+  nodes.emplace();
+  // Assumes we've already put all 1-value stones into the queue
+  do {
+    auto next = nodes.front();
+    nodes.pop();
+
+    const auto nextStone = next.max + 1;
+    for (const auto &move : next.allPossibleMoves()) {
+      Node newNode = next;
+      ensure(newNode.board[move] == 0);
+
+      if (newNode.sum(move) == nextStone) {
+        newNode.apply(move, nextStone);
+        nodes.push(newNode);
+
+        if (nextStone > highestNode.max) {
+          highestNode = newNode;
+        }
+      }
+    }
+  } while (!nodes.empty());
+
+  return highestNode.max;
 }
